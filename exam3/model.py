6a689aaab3b649694614abd2e3b2f006ef0685c4
Ray Chen <ray941216@gmail.com>
2020-06-17 02:24:55 +0800

    answer w/ training model

--- exam3/model.py
+++ exam3/model.py
@@ -1,5 +1,80 @@
+import math
+import os
+import pickle
+
+import pandas as pd
+import torch
 import torch.nn as nn
+from sklearn.preprocessing import OneHotEncoder
+from torch.nn import functional as F
+from torch.utils.data import Dataset
+from tqdm import tqdm
 
 
 class Model(nn.Module):
-    pass
+    def __init__(self, features):
+        super(Model, self).__init__()
+        self.lstm_in = nn.LSTM(
+            input_size=features, hidden_size=3, batch_first=True
+        )
+        self.dnn_out = nn.Linear(
+            in_features=3, out_features=features
+        )
+
+    def forward(self, x):
+        output, (ht, ct) = self.lstm_in(x)
+        return F.softmax(self.dnn_out(F.relu(ht.view(-1, ht.size(-1)))), dim=-1)
+class dataset(Dataset):
+    def __init__(self, window_size=5, step_size=1, task="train", evaluate_ratio=0.2):
+        super().__init__()
+        self.window_size = window_size
+        self.step_size = step_size
+
+        if os.path.exists("./onehotencoder.pkl"):
+            with open("./onehotencoder.pkl", "rb") as f:
+                self.onehot = pickle.load(f)
+        else:
+            self.onehot = OneHotEncoder(handle_unknown="ignore")
+            self.onehot.fit(
+                pd.read_csv("../exam2/data/train_need_aggregate.csv")["EventId"]
+                .unique()
+                .reshape(-1, 1)
+            )
+            with open("./onehotencoder.pkl", "wb") as f:
+                pickle.dump(self.onehot, f)
+
+        data_to_load = "test" if task == "test" else "train"
+        self.data = self.onehot.transform(
+            pd.read_csv(f"../exam2/data/{data_to_load}_need_aggregate.csv")[
+                "EventId"
+            ].values.reshape(-1, 1)
+        ).toarray()
+
+        if task == "train":
+            self.start_idx = 0
+            self.end_idx = math.floor(self.data.shape[0] * (1 - evaluate_ratio))
+        elif task == "evaluate":
+            self.start_idx = (
+                math.floor(self.data.shape[0] * (1 - evaluate_ratio)) - window_size
+            )
+            self.end_idx = self.data.shape[0]
+        elif task == "test":
+            self.start_idx = 0
+            self.end_idx = self.data.shape[0]
+
+    def __getitem__(self, n):
+        if n < 0:
+            n += self.__len__()
+
+        x = self.start_idx + n * self.step_size
+        return {
+            "x": torch.tensor(self.data[x : x + self.window_size], dtype=torch.float),
+            # "y": torch.tensor(self.data[x + self.window_size], dtype=torch.float),
+            "y": torch.tensor((self.onehot.inverse_transform(self.data[x + self.window_size].reshape(1, -1)) - 1), dtype=torch.long),
+        }
+
+    def __len__(self):
+        return (self.end_idx - self.start_idx - self.window_size) // self.step_size
+
+    def features(self):
+        return self.__getitem__(0)["x"].size(-1)
--- exam3/train.py
+++ exam3/train.py
@@ -1,5 +1,91 @@
-from model import Model
+import click
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+from model import Model, dataset
+
+
+@click.command()
+@click.help_option('-h', '--help')
+@click.option('--epochs', type=click.INT, default=20, help='Number of epochs.')
+@click.option('--window', type=click.INT, default=22, help='Number of window size.')
+@click.option('--step', type=click.INT, default=1, help='Number of step size.')
+def train_model(epochs=10, window=22, step=1):
+    dataset_tr = dataset(task='train', window_size=window, step_size=step)
+    dataset_ev = dataset(task='evaluate', window_size=window, step_size=step)
+    model = Model(dataset_tr.features())
+    print(f'model built! It will training on {len(dataset_tr)} seqs and evaluate on {len(dataset_ev)} seqs')
+    print(model)
+    print(f"Trainable Perameters: {sum(p.numel() for p in model.parameters())}\n")
+
+    opt = optim.Adam(model.parameters(), lr=1e-4)
+    criterion = nn.CrossEntropyLoss()
+
+    d_loader_tr = DataLoader(
+        dataset=dataset_tr,
+        shuffle=True,
+        num_workers=2,
+        batch_size = 128
+    )
+
+    d_loader_ev = DataLoader(
+        dataset=dataset_ev,
+        shuffle=True,
+        num_workers=2,
+        batch_size = 1024
+    )
+
+    losses = {'train': [], 'eval': []}
+    best_loss = 1e9
+    no_improve = 0
+    for epoch in range(epochs):
+        with tqdm(d_loader_tr, f'Epoch-#{epoch + 1}') as training:
+            model.train()
+            losses_tr = []
+            for s, batch in enumerate(training):
+                opt.zero_grad()
+
+                out = model(batch['x'])
+
+                loss = criterion(out, batch['y'].view(-1))
+                loss.backward()
+                opt.step()
+
+                training.set_description(f'Epoch-#{epoch + 1} Loss={loss.item():.4f}')
+                losses_tr.append(loss.item())
+            losses['train'].append(np.mean(losses_tr))
+
+        with torch.no_grad():
+            model.eval()
+            losses_ev = []
+            for s, batch in enumerate(tqdm(d_loader_ev, 'Evaluating')):
+                out = model(batch['x'])
+                loss = criterion(out, batch['y'].view(-1))
+                losses_ev.append(loss.item())
+            losses['eval'].append(np.mean(losses_ev))
+
+        print(f"Epoch-#{epoch + 1} AVG training loss: {losses['train'][-1]:.4f} / evaluation loss: {losses['eval'][-1]:.4f}")
+        if losses['eval'][-1] <= best_loss:
+            print(f"Evaluation loss improved form {best_loss} to {losses['eval'][-1]}, save best model.\n")
+            best_loss = losses['eval'][-1]
+            torch.save(model.state_dict(), './model')
+            no_improve = 0
+        else:
+            no_improve += 1
+            print(f'Model no improve for {no_improve} times.')
+
+
+        if no_improve == 10:
+            print('Model no improve reach 10 times. Early Stop!')
+            break
+
+
+    print('Training finished!')
+
 
 if __name__ == '__main__':
-    model = Model()
-    pass
+    train_model()
