c4e2760bb2f0145ea6cfabaaff2999436aae60b1
Ray Chen <ray941216@gmail.com>
2020-06-17 14:32:14 +0800

    answer predict anomaly

--- exam3/predict.py
+++ exam3/predict.py
@@ -1,7 +1,79 @@
 import os
 
-if __name__ == '__main__':
-    if not os.path.exists('result'):
-        os.mkdir('result')
+import numpy as np
+import pandas as pd
+import pendulum as pdl
+import swifter
+import torch
+from torch.utils.data import DataLoader
 
-    pass
+from model import Model, dataset
+
+if __name__ == "__main__":
+    if not os.path.exists("result"):
+        os.mkdir("result")
+
+    win = 22
+
+    dataset_te = dataset(task="test", window_size=win)
+    model = Model(dataset_te.features())
+
+    if os.path.exists("./model/model_best"):
+        model.load_state_dict(
+            torch.load("./model/model_best", map_location=torch.device("cpu"))
+        )
+    elif os.path.exists("./model/model"):
+        model.load_state_dict(
+            torch.load("./model/model", map_location=torch.device("cpu"))
+        )
+    else:
+        print("no trained model to load")
+
+    loader_te = DataLoader(
+        dataset=dataset_te, shuffle=False, num_workers=1, batch_size=1024
+    )
+
+    report = pd.read_csv("../exam2/data/test_need_aggregate.csv")
+
+    with torch.no_grad():
+        model.eval()
+        pred = []
+        for s, batch in enumerate(loader_te):
+            pred.append(model(batch["x"]))
+
+        prob = torch.cat(pred, dim=0)
+
+    anomaly_prob = []
+    confidence = []
+
+    for record, p in zip(list(report.EventId[win:] - 1), prob.numpy()):
+        anomaly_prob.append((1 + max(p) - 2 * p[record]) / 2)
+        confidence.append((1 - max(p)))
+
+    report["conf"] = [1 for i in range(win)] + confidence
+    report["anomaly_prob"] = [0 for i in range(win)] + anomaly_prob
+
+    report["anomaly"] = [0 for x in report.index]
+    report.loc[
+        (
+            (report["anomaly_prob"] > np.quantile(report["anomaly_prob"], 0.997))
+            & (report["anomaly_prob"] > 0.7)
+        ),
+        "anomaly",
+    ] = 1
+
+    report["datetime"] = report["datetime"].swifter.apply(
+        lambda x: pdl.parse(x).format("YYYY-MM-DD HH:mm:00")
+    )
+
+    report_agg = (
+        report.groupby("datetime")["anomaly"]
+        .apply(list)
+        .apply(max)
+        .reset_index(name="anomaly")
+    )
+
+    pd.merge(
+        pd.read_csv("../exam2/result/test.csv"), report_agg, how="left", on=["datetime"]
+    ).to_csv("./result/predict.csv", index=False)
+
--- exam3/train.py
+++ exam3/train.py
@@ -1,3 +1,5 @@
+import os
+
 import click
 import numpy as np
 import torch
@@ -10,15 +12,20 @@ from model import Model, dataset
 
 
 @click.command()
-@click.help_option('-h', '--help')
-@click.option('--epochs', type=click.INT, default=20, help='Number of epochs.')
-@click.option('--window', type=click.INT, default=22, help='Number of window size.')
-@click.option('--step', type=click.INT, default=1, help='Number of step size.')
+@click.help_option("-h", "--help")
+@click.option("--epochs", type=click.INT, default=20, help="Number of epochs.")
+@click.option("--window", type=click.INT, default=22, help="Number of window size.")
+@click.option("--step", type=click.INT, default=1, help="Number of step size.")
 def train_model(epochs=10, window=22, step=1):
-    dataset_tr = dataset(task='train', window_size=window, step_size=step)
-    dataset_ev = dataset(task='evaluate', window_size=window, step_size=step)
+    if not os.path.isdir('./model'):
+        os.mkdir('./model')
+
+    dataset_tr = dataset(task="train", window_size=window, step_size=step)
+    dataset_ev = dataset(task="evaluate", window_size=window, step_size=step)
     model = Model(dataset_tr.features())
-    print(f'model built! It will training on {len(dataset_tr)} seqs and evaluate on {len(dataset_ev)} seqs')
+    print(
+        f"model built! It will training on {len(dataset_tr)} seqs and evaluate on {len(dataset_ev)} seqs"
+    )
     print(model)
     print(f"Trainable Perameters: {sum(p.numel() for p in model.parameters())}\n")
 
@@ -26,66 +33,62 @@ def train_model(epochs=10, window=22, step=1):
     criterion = nn.CrossEntropyLoss()
 
     d_loader_tr = DataLoader(
-        dataset=dataset_tr,
-        shuffle=True,
-        num_workers=2,
-        batch_size = 128
+        dataset=dataset_tr, shuffle=True, num_workers=2, batch_size=128
     )
 
     d_loader_ev = DataLoader(
-        dataset=dataset_ev,
-        shuffle=True,
-        num_workers=2,
-        batch_size = 1024
+        dataset=dataset_ev, shuffle=True, num_workers=2, batch_size=1024
     )
 
-    losses = {'train': [], 'eval': []}
+    losses = {"train": [], "eval": []}
     best_loss = 1e9
     no_improve = 0
     for epoch in range(epochs):
-        with tqdm(d_loader_tr, f'Epoch-#{epoch + 1}') as training:
+        with tqdm(d_loader_tr, f"Epoch-#{epoch + 1}") as training:
             model.train()
             losses_tr = []
             for s, batch in enumerate(training):
                 opt.zero_grad()
 
-                out = model(batch['x'])
+                out = model(batch["x"])
 
-                loss = criterion(out, batch['y'].view(-1))
+                loss = criterion(out, batch["y"].view(-1))
                 loss.backward()
                 opt.step()
 
-                training.set_description(f'Epoch-#{epoch + 1} Loss={loss.item():.4f}')
+                training.set_description(f"Epoch-#{epoch + 1} Loss={loss.item():.4f}")
                 losses_tr.append(loss.item())
-            losses['train'].append(np.mean(losses_tr))
+            losses["train"].append(np.mean(losses_tr))
 
         with torch.no_grad():
             model.eval()
             losses_ev = []
-            for s, batch in enumerate(tqdm(d_loader_ev, 'Evaluating')):
-                out = model(batch['x'])
-                loss = criterion(out, batch['y'].view(-1))
+            for s, batch in enumerate(tqdm(d_loader_ev, "Evaluating")):
+                out = model(batch["x"])
+                loss = criterion(out, batch["y"].view(-1))
                 losses_ev.append(loss.item())
-            losses['eval'].append(np.mean(losses_ev))
+            losses["eval"].append(np.mean(losses_ev))
 
-        print(f"Epoch-#{epoch + 1} AVG training loss: {losses['train'][-1]:.4f} / evaluation loss: {losses['eval'][-1]:.4f}")
-        if losses['eval'][-1] <= best_loss:
-            print(f"Evaluation loss improved form {best_loss} to {losses['eval'][-1]}, save best model.\n")
-            best_loss = losses['eval'][-1]
-            torch.save(model.state_dict(), './model')
+        print(
+            f"Epoch-#{epoch + 1} AVG training loss: {losses['train'][-1]:.4f} / evaluation loss: {losses['eval'][-1]:.4f}"
+        )
+        if losses["eval"][-1] <= best_loss:
+            print(
+                f"Evaluation loss improved form {best_loss} to {losses['eval'][-1]}, save best model.\n"
+            )
+            best_loss = losses["eval"][-1]
+            torch.save(model.state_dict(), "./model/model")
             no_improve = 0
         else:
             no_improve += 1
-            print(f'Model no improve for {no_improve} times.')
-
+            print(f"Model no improve for {no_improve} times.")
 
         if no_improve == 10:
-            print('Model no improve reach 10 times. Early Stop!')
+            print("Model no improve reach 10 times. Early Stop!")
             break
 
+    print("Training finished!")
 
-    print('Training finished!')
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     train_model()
