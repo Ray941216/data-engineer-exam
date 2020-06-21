2685e8e35b523c05b9698800fecbdfd859908ead
Ray Chen <ray941216@gmail.com>
2020-06-21 18:35:09 +0800

    improve the efficiency

--- exam2/main.py
+++ exam2/main.py
@@ -1,47 +1,56 @@
 import os
+
 import pandas as pd
 import swifter
 
-import pendulum as pdl
 
-if __name__ == '__main__':
-    if not os.path.exists('result'):
-        os.mkdir('result')
+def agg(filename="train"):
+    if not os.path.exists(f"./data/{filename}_need_aggregate.csv"):
+        raise FileNotFoundError
 
-    df_tr = pd.read_csv('./data/train_need_aggregate.csv')
-    df_te = pd.read_csv('./data/test_need_aggregate.csv')
+    df = pd.read_csv(f"./data/{filename}_need_aggregate.csv")
 
-    df_tr['datetime'] = (
-        df_tr['datetime']
-        .swifter
-        .apply(
-            lambda x: pdl.parse(x).format('YYYY-MM-DD HH:mm:00')
-        )
-    )
+    result = {}
+    for x in df.itertuples():
+        dt = ":".join(x[1].split(":")[:-1] + ["00"])
+        if dt in result:
+            result[dt].append(x[2])
+        else:
+            result[dt] = [x[2]]
 
-    df_te['datetime'] = (
-        df_te['datetime']
-        .swifter
-        .apply(
-            lambda x: pdl.parse(x).format('YYYY-MM-DD HH:mm:00')
-        )
-    )
-
-    df_tr_agg = (
-        df_tr
-        .groupby('datetime')['EventId']
-        .apply(list)
-        .reset_index(name='EventId')
-        .to_csv('./result/train.csv', index=False)
-    )
-
-    df_te_agg = (
-        df_te
-        .groupby('datetime')['EventId']
-        .apply(list)
-        .reset_index(name='EventId')
-        .to_csv('./result/test.csv', index=False)
+    pd.DataFrame(result.items(), columns=df.columns).to_csv(
+        f"./result/{filename}.csv", index=False
     )
 
 
+def parser(x):
+    return ":".join(x.split(":")[:-1] + ["00"])
 
+
+if __name__ == "__main__":
+    if not os.path.exists("result"):
+        os.mkdir("result")
+
+    agg("train")
+    agg("test")
+
+    # df_tr = pd.read_csv("./data/train_need_aggregate.csv")
+    # df_te = pd.read_csv("./data/test_need_aggregate.csv")
+
+    # df_tr["datetime"] = df_tr["datetime"].swifter.apply(parser)
+
+    # df_te["datetime"] = df_te["datetime"].swifter.apply(parser)
+
+    # df_tr_agg = (
+    #     df_tr.groupby("datetime")["EventId"]
+    #     .apply(list)
+    #     .reset_index(name="EventId")
+    #     .to_csv("./result/train.csv", index=False)
+    # )
+
+    # df_te_agg = (
+    #     df_te.groupby("datetime")["EventId"]
+    #     .apply(list)
+    #     .reset_index(name="EventId")
+    #     .to_csv("./result/test.csv", index=False)
+    # )
